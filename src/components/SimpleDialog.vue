a8a7cb5778ad61ac8a63da3a93ac6bd840e91cf9
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2020-03-23 15:41:08 +0100

    update - store, defaults, coockies, add - dialog-component, success message

---
+++ src/components/SimpleDialog.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-dialog
+      v-model="dialog.toggle"
+      max-width="290"
+    >
+      <v-card>
+        <v-card-title class="headline">{{dialog.title}}</v-card-title>
+
+        <v-card-text>
+          <div v-html="dialog.msg"></div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dismiss"
+          >
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import store from '../store'
+export default Vue.extend({
+    name: 'SimpleDialog',
+    computed: {
+        dialog () {
+            return store.state.simpleDialog;
+        }
+    },
+    methods: {
+        dismiss () {
+            store.commit('simpleDialogToggle', false);
+        }
+    }
+})
+</script>
