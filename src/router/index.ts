920b078cbdf6183252148f05d093bfcff9a1aea9
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2020-03-30 17:21:41 +0200

    add - page meta data, favicon

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,12 +9,18 @@ const routes = [
     path: '/',
     redirect: '/survey',
     name: 'Home',
-    component: Login
+    component: Login,
+    meta: {
+      title: 'Corono Routine Monitoring - HCU Hamburg',
+    }
   },
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: {
+      title: 'Corono Routine Monitoring - HCU Hamburg',
+    }
   },
   // {
   //   path: '/sign-up',
@@ -22,11 +28,17 @@ const routes = [
   // },
   {
     path: '/imprint',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Imprint.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Imprint.vue'),
+    meta: {
+      title: 'CoRoMo - Impressum',
+    }
   },
   {
     path: '/generalinfo',
-    component: () => import(/* webpackChunkName: "about" */ '../views/GeneralInfo.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/GeneralInfo.vue'),
+    meta: {
+      title: 'CoRoMo - Registrierung',
+    }
   },
   {
     path: '/survey',
@@ -34,12 +46,53 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Survey.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Survey.vue'),
+    meta: {
+      title: 'Corono Routine Monitoring - HCU Hamburg',
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
-})
+});
+
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if(nearestWithTitle) document.title = nearestWithTitle.meta.title;
+
+  // Remove any stale meta tags from the document using the key attribute we set below.
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+
+  // Skip rendering meta tags if there are none.
+  if(!nearestWithMeta) return next();
+
+  // Turn the meta tag definitions into actual elements in the head.
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta');
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key]);
+    });
+
+    // We use this to track which meta tags we create, so we don't interfere with other ones.
+    tag.setAttribute('data-vue-router-controlled', '');
+
+    return tag;
+  })
+  // Add the meta tags to the document head.
+  .forEach(tag => document.head.appendChild(tag));
+
+  next();
+});
 
 export default router
